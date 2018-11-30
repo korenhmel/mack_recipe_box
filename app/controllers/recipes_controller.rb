92dc1241be62bdaad24ac5978868ea03579cd91c
koren <koren.hmel@gmail.com>
2018-11-30 14:39:35 +0200

    add gem simpl form and bootstrap and create _form.haml

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,36 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  def index
+    @recipe = Recipe.all
+  end
+
+   def show
+   end
+
+   def new
+    @recipe = Recipe.new
+   end
+
+   def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Successfully created new recipe"
+    else
+      render 'new'
+    end
+   end
+
+   def destroy
+   end
+
+   private
+   def set_recipe
+    @recipe = Recipe.find(params[:id])
+   end
+
+   def recipe_params
+    params.require(:recipe).permit(:title, :description, :user_id)
+   end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :recipes
+  root :to => "recipes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
