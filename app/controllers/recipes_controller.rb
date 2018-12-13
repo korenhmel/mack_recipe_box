9a50c2695773ddfab23f81d8c2f40f1f6a9d4326
koren <koren.hmel@gmail.com>
2018-12-13 12:48:34 +0200

    add gem paperclip, and change style in app.css

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -44,7 +44,7 @@ class RecipesController < ApplicationController
    end
 
    def recipe_params
-    params.require(:recipe).permit(:title, :description)
+    params.require(:recipe).permit(:title, :description, :image)
    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181127121452) do
+ActiveRecord::Schema.define(version: 20181211160924) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20181127121452) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
